6badc12691208cae0b981884b1b99d32ddbeb471
AA <alexdev73@gmail.com>
2021-11-18 18:26:47 +0100

    Commit party :-) (everything work)(almost)

--- js/script.js
+++ js/script.js
@@ -16,4 +16,19 @@ $("#btn").click(function(){
       $("body").css("background-color","rgb(0,0,128)");
       $(".ok").css("display","none");
       $(".blue-screen").css("display","block");
+      var audio = new Audio('./audio/bluescreensound.mp3');
+      audio.play();
     })
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+      
